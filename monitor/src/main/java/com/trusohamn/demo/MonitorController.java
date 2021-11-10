2fd9fbe280317ccb01d9ba12fe8c0e491911bfc8
Marta Trusohamn <marta.trusohamn@gmail.com>
2021-11-10 22:53:55 +0100

    added cors on server

--- monitor/src/main/java/com/trusohamn/demo/MonitorController.java
+++ monitor/src/main/java/com/trusohamn/demo/MonitorController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 class MonitorController {
 
   private final ServiceRepository repository;
