7bb8416edb013d60294e46302dbbfd71b2692ce7
Marta Trusohamn <marta.trusohamn@gmail.com>
2021-11-11 22:07:27 +0100

    typo in delete

--- monitor/src/main/java/com/trusohamn/demo/MonitorController.java
+++ monitor/src/main/java/com/trusohamn/demo/MonitorController.java
@@ -1,7 +1,6 @@
 package com.trusohamn.demo;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -63,7 +62,7 @@ class MonitorController {
     return repository.findById(id).orElseThrow(() -> new RuntimeException());
   }
 
-  @DeleteMapping("/Services/{id}")
+  @DeleteMapping("/services/{id}")
   void deleteService(@PathVariable Long id) {
     repository.deleteById(id);
   }
